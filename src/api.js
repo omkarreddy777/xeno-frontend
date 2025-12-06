40140db6f11113591f67301e8585b76006276a14
omkarreddy777 <omkarreddy777@gmail.com>
2025-12-06 23:52:28 +0530

    Updated API baseURL for Railway deployment

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "https://xeno-backend-production-da6d.up.railway.app/",
 });
 
 API.interceptors.request.use((config) => {
